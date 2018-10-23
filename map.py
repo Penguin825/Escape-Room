be00ac649368b5b67fe4d60a79cbadaad67f6ea2
PetercSnk <pcsnook@btinternet.com>
2018-10-23 14:14:38 +0100

    map changes

--- map.py
+++ map.py
@@ -4,10 +4,10 @@ room_cupboard = {
     "name": "Cupboard",
 
     "description":
-    """You are currently in a cupboard in a house you are stuck. Your
-challenge is to overcome the obstacles and get out of the house.
-The cupboard has an exit to the east and a pile of wired coat hangers
-to the west.""",
+    """You are currently stuck in a cupboard on the second floor of a strange house.
+Your challenge is to overcome the obstacles and get out of the house.
+The cupboard has an exit to the east and some wired coat hangers
+in the corner of the room.""",
 
     "exits": {"east": "Bedroom"},
 
@@ -18,10 +18,10 @@ room_bedroom = {
     "name": "Bedroom",
 
     "description":
-    """You are stood in the middle of the bedroom. The exit to the west leads
-you back into the cupboard. To the south is locked and cannot be
-opened. The exit to the east leads to the bathroom but is also locked but can
-be picked using wire.""",
+    """You are standing in the middle of a bedroom. There is an exit to
+the west which leads you back into the cupboard. To the south is another door which cannot be opened.
+The exit to the east leads to what looks like an on-suit bathroom, this door is also locked. With the
+right tools this door can be picked.""",
 
     "exits":  {"west": "Cupboard" , "east": "Bathroom"},
 
@@ -32,9 +32,9 @@ room_bathroom = {
     "name": "Bathroom",
 
     "description":
-    """You are now in a dirty bathroom. There is a sign to the north that says KEEP
-THIS BATHROOM CLEAN! Go west to re-enter the bedroom. The exit to the south
-is an exit into the corridor.""",
+    """You are now in a dirty bathroom. There is a sign on the tiled wall that says KEEP
+THIS BATHROOM CLEAN OR ESLE! West leads back to the bedroom. The exit to the south
+is an exit into a corridor.""",
 
     "exits": {"west": "Bedroom", "south": "Upstairs corridor"},
 
@@ -46,11 +46,13 @@ room_corridor = {
 
     "description":
     """You are now in the middle of the upstairs corridor. The exit to the south
-are the stairs going downstairs. However, there is a dog patterned stair gate
-with a four lettered lock code. The code to the stair gate is somewhere in the
-corridor. There is also an exit to the north leading to the bedroom; to the
-east there is a door to the study but it is only accessible using a card touch
-pad. There is a a dog painting on the west wall.""",
+is a flight of stairs going down. However, there is a dalmatian patterned stair gate
+with a four lettered lock code. You also wish that there is a cute dalmation dog somewhere
+to comfort you. The code to the stair gate could be somewhere in the
+corridor. The exit to the north leads back to the bedroom; to the
+east there is a door to a study but it is only accessible using a smart card.
+There is also a really cute dog painting on the wall, The longer you stare at the painting
+the more happy you become.""",
 
     "exits": {"east": "Study", "south": "Stairs", "north": "Bedroom"},
 
@@ -61,9 +63,9 @@ room_study = {
     "name": "Study",
 
     "description":
-    """You have now accessed the study. The walls are lined with book shelves and there
-is a desk in the centre of the room. There is an exit to the west to return into
-the upstairs corridor.""",
+    """You have now gained access to the study. The walls are lined with book shelves, you see alot
+of books named 'Java Puns', you are definitely better off not reading them. There
+is a large desk in the centre of the room. The exit to the west leads back to the upstairs corridoor.""",
 
     "exits": {"west": "Upstairs corridor"},
 
@@ -74,8 +76,8 @@ room_stairs = {
     "name": "Stairs",
 
     "description":
-    """You are on the stairs. Go north to the upstairs corridor or south to the
-downstairs hallway.""",
+    """You are on the fluffy carpeted stairs. You can go north back to the upstairs corridor
+or south to the eerie downstairs hallway.""",
 
     "exits": {"north": "Upstairs corridor", "south": "Hallway"},
 
@@ -86,9 +88,9 @@ room_kitchen = {
     "name": "Kitchen",
 
     "description":
-    """You are now in the kitchen. There is a crow bar and some dirty plates on the
-breakfast bar. Go east to return to the hallway. The exit to the south leads to
-dining room and is currently jammed and therefore needs to be forced open.""",
+    """You are now in a kitchen. You are especially annoyed by the dirty plates left
+on the breakfast bar, also why is there a crowbar here? You can go east to return to the hallway.
+The exit to the south leads to a dining room and is currently jammed, maybe it can be forced open.""",
 
     "exits": {"south": "Dining Room", "east": "Hallway"},
 
@@ -100,9 +102,10 @@ room_dining = {
     "name": "Dining Room",
 
     "description":
-    """ You are now in the living room. There is a laptop on the dining room
-table as well as a vase of flowers. There is an exit to the north which leads
-to the kitchen.""",
+    """You are now in a dining room. There is a alienware laptop on the dining room
+table. Alienware? You think to yourself who in their right mind would purchase that overpriced #£$%.
+There is also a pretty vase of flowers, whoever picked those has good taste.
+The exit to the north leads back to the kitchen.""",
 
     "exits": {"north": "Kitchen"},
 
@@ -114,8 +117,8 @@ room_living = {
     "name": "Living Room",
 
     "description":
-    """You are currently in the living room. There are two sofas, a TV and
-a coffee table. The coffe table has an ID card and a cup of tea. There is only
+    """You are currently in a living room. There is one large corner sofa, a TV and
+a coffee table. The coffe table has an ID card and a cold cup of tea left on it. There is only
 one exit which is to the west and returns you to the hallway.""",
 
     "exits": {"west": "Hallway"},
@@ -129,7 +132,7 @@ room_hallway = {
 
     "description":
     """You are now in the downstairs hallway. There is an exit to the west which
-leads to the lvining room. The exit to the east leads to the kitchen and to the
+leads to the living room. The exit to the east leads to the kitchen and
 south there is the front door. However, the front door is locked and cannot be 
 opened without key.""",
 
